c60ebf5c30516f405d7d3b09863f1a6a39ab5726
Dang Khoa <ojtgrowth@sendo.vn>
2025-09-12 13:21:34 +0700

    feat: delete the folder core in the services.

--- services/index.ts
+++
@@ -1,8 +0,0 @@
-// core
-export * from "./core/query-keys";
-export * from "./core/errors";
-// (thường không export http trực tiếp ra ngoài để tránh lạm dụng)
-
-// domains
-export * as Auth from "./auth/hooks";
-export * as Products from "./products/hooks";
