1608ad70a0d790d4aeae9e3cf0682ed1cac7945b
hokhoi1979 <166026796+hokhoi1979@users.noreply.github.com>
2025-11-24 23:49:10 +0700

    fix

--- services/product/getProduct/api.ts
+++ services/product/getProduct/api.ts
@@ -1,7 +1,7 @@
 import api from "@/config/axios";
 import { GetAllProductAdminResponse } from "./type";
 
-export async function getAllProductAdmin(): Promise<GetAllProductAdminResponse> {
+export async function getAllProductAdmin(): Promise<ProductAdmin[]> {
   const { data } = await api.get<GetAllProductAdminResponse>("/products");
-  return data;
+  return data.data;
 }
